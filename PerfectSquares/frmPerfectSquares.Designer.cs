29d656935af1a63183ec348bb987e65bd81d5d00
Sam-Morency <sam.morency@ocsbstudent.ca>
2015-10-13 13:16:18 -0400

    PerfectSquaresDesign

---
+++ PerfectSquares/frmPerfectSquares.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PerfectSquares
+{
+    partial class frmPerfectSquares
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lstAnswers = new System.Windows.Forms.ListBox();
+            this.nudSquareRoot = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSquareRoot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(5, 111);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(141, 39);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Enter a number and I will\r\ngive you the perfect squares\r\nbelow it.";
+            // 
+            // lstAnswers
+            // 
+            this.lstAnswers.FormattingEnabled = true;
+            this.lstAnswers.Location = new System.Drawing.Point(152, 39);
+            this.lstAnswers.Name = "lstAnswers";
+            this.lstAnswers.ScrollAlwaysVisible = true;
+            this.lstAnswers.Size = new System.Drawing.Size(120, 212);
+            this.lstAnswers.TabIndex = 2;
+            // 
+            // nudSquareRoot
+            // 
+            this.nudSquareRoot.Location = new System.Drawing.Point(152, 13);
+            this.nudSquareRoot.Name = "nudSquareRoot";
+            this.nudSquareRoot.Size = new System.Drawing.Size(120, 20);
+            this.nudSquareRoot.TabIndex = 3;
+            // 
+            // frmPerfectSquares
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.nudSquareRoot);
+            this.Controls.Add(this.lstAnswers);
+            this.Controls.Add(this.lblInstructions);
+            this.Name = "frmPerfectSquares";
+            this.Text = "Perfect Squares";
+            this.Load += new System.EventHandler(this.frmPerfectSquares_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudSquareRoot)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.ListBox lstAnswers;
+        private System.Windows.Forms.NumericUpDown nudSquareRoot;
+    }
+}
+
---
+++ PerfectSquares/frmPerfectSquares.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquares
+{
+    public partial class frmPerfectSquares : Form
+    {
+        public frmPerfectSquares()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPerfectSquares_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
